9e6f5704e101d4133073c0db07fefb528b880412
monika <monikax1221@gmail.com>
2018-06-23 17:37:06 +0200

    initial commit

---
+++ src/clicker/LocationController.java
@@ -0,0 +1,16 @@
+package clicker;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class LocationController {
+
+    @FXML
+    Button firstLocationButton;
+
+    @FXML
+    public void firstLocationButton() {
+        System.out.println("Warsaw button has been clicked");
+    }
+
+}
---
+++ src/clicker/MainController.java
@@ -0,0 +1,32 @@
+package clicker;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
+
+public class MainController {
+    @FXML
+    private Button enterButton;
+
+    private Stage stage;
+
+    @FXML
+    public void enterGame() throws IOException {
+        System.out.println("Game has been entered");
+
+        stage = (Stage) enterButton.getScene().getWindow();
+        AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("../resources/view/locationView.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+
+    }
+
+
+}
